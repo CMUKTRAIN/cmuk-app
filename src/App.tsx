8567147365cb120f41a816e36cc238163687de22
Devin-147 <devinscannura@icloud.com>
2026-06-27 17:22:34 -0400

    Add new paragraph for Today's Chefs in Training

--- src/App.tsx
+++ src/App.tsx
@@ -116,6 +116,8 @@ export default function App() {
               </div>
               <p className="text-[10px] font-bold text-brand-orange uppercase tracking-widest leading-none mt-1">
                 Fuel Your Future
+              </p>
+               <p className="text-[10px] font-bold text-brand-orange uppercase tracking-widest leading-none mt-1">
                 Today's Chefs in Training
               </p>
             </div>
