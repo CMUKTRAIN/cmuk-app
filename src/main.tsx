87f0e3bb00f6982243d960eb3d117a359957d145
Devin-147 <devinscannura@icloud.com>
2026-09-04 12:51:07 -0400

    Replace App component with Router and add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
-import App from './App.tsx';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './Router.tsx';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <BrowserRouter>
+      <Router />
+    </BrowserRouter>
+  </StrictMode>
 );
